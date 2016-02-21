c71db87a0bcabb5129912033cfbc44706ba2ac88
Plum <stephanie.plumeri@gmail.com>
2016-02-21 17:16:40 -0500

    updating port for virtual hosts

--- bin/www.js
+++ bin/www.js
@@ -12,7 +12,7 @@ var app = require('../app'),
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '3002');
 app.set('port', port);
 
 /**
